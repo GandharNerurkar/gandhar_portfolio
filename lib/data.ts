ca417847f53c7372f844d36a218b8bcc61d2ebe7
GandharNerurkar <gandhar.nerurkar@zapcg.com>
2025-06-24 12:12:48 +0530

    Project section updated

--- lib/data.ts
+++ lib/data.ts
@@ -5,6 +5,8 @@ import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
+import EcommerceImg from "../assets/e-commece.png";
+import Portfolio from "../assets/portfolio.png"
 
 export const links = [
   {
@@ -62,25 +64,18 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Shopify",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "Developed a full-stack eCommerce application. Implemented product search, cart functionality, and an admin panel for category-based product management.",
+    tags: ["React", "MongoDB", "Tailwind", "Node"],
+    imageUrl: EcommerceImg,
   },
   {
-    title: "rmtDev",
+    title: "Portfolio",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "Designed and developed a personal portfolio using modern web technologies to showcase my projects, skills, and experience.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Framer Motion"],
+    imageUrl: Portfolio,
   },
 ] as const;
 
